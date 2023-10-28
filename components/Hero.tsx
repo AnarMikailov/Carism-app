28cf8dd23f4a3392a982f419817a453caf113901
AnarMikailov <a.v.mikayilov@gmail.com>
2023-10-28 21:12:32 +0400

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
+import Image from "next/image";
 
-import { CustomButton } from '../components';
+import { CustomButton } from "../components";
 
 const Hero = () => {
   const handleScroll = () => {
-    const nextSection = document.getElementById('discover');
+    const nextSection = document.getElementById("discover");
 
     if (nextSection) {
-      nextSection.scrollIntoView({ behavior: 'smooth' });
+      nextSection.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -17,7 +17,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+          Find, booooooook, rent a car—quick and super easy!
         </h1>
 
         <p className="hero__subtitle">
